9e7903603274dd95962c362852f0d4ee836310c1
Enzo Leon <enzo.leon77@gmail.com>
2024-04-15 10:40:54 +0200

    first commit

---
+++ Livre.py
@@ -0,0 +1,9 @@
+class Livre:
+    def __init__(self,auteur, titre, contenu) -> None:
+        self.auteur = auteur
+        self.titre = titre
+        self.contenu = contenu
+
+    def AddLivre():
+        
+
